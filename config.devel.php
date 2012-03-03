4c7e88871e960c6710957894edc899fe47c41449
Gabriele Tozzi <gabriele@tozzi.eu>
2012-03-03 16:19:06 +0100

    Licensing

--- config.devel.php
+++ config.devel.php
@@ -1,6 +1,26 @@
 <?php
 
-// main configuration file
+/**
+* @file config.php Main configuration file
+*
+* @author Gabriele Tozzi <gabriele@tozzi.eu>
+* @date   03.03.2012
+*
+* This file is part of DWallet
+*
+* DWallet is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* DWallet is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 $conf['debug'] = true;
 
